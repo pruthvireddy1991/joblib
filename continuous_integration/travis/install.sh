2c01e6805431e63767745000cd3c3e8859898704
pruthvireddy1991 <42368140+pruthvireddy1991@users.noreply.github.com>
2020-01-02 14:53:53 +0530

    Update install.sh

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -44,7 +44,6 @@ create_new_conda_env() {
     # Use the miniconda installer for faster download / install of conda
     # itself
     UNAME_ARCH=$(uname -m)
-    SUDO = ""
     MINICONDA_PREFIX=/home/travis/miniconda
     if [ "$UNAME_ARCH" == 'aarch64' ]; then
       wget -q "https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh" -O archiconda.sh;
@@ -55,6 +54,7 @@ create_new_conda_env() {
       wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
          -O miniconda.sh;
       chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PREFIX;
+      SUDO='';
     fi
     export PATH=$MINICONDA_PREFIX/bin:$PATH
     $SUDO conda update --yes conda
